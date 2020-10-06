7036c80c41abecf26bddccfc56a3e36ae74637b7
Siarhei Zaretsky <z__s@mail.ru>
2020-10-06 22:34:27 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = ''
+    let letterEncoded
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        letterEncoded = expr.substr(i, 10)
+
+        let code = ''
+        for (let j = 0; j < 10; j += 2) {
+            let symbol = letterEncoded.substr(j, 2)
+            switch (symbol) {
+                case '10':
+                    code += '.'
+                    break
+                case '11':
+                    code += '-'
+                    break
+            }
+        }
+
+        if (code !== '' && MORSE_TABLE.hasOwnProperty(code)) {
+            res += MORSE_TABLE[code]
+        } else if (letterEncoded == '**********') {
+            res += ' '
+        }
+    }
+
+    return res
 }
 
 module.exports = {
